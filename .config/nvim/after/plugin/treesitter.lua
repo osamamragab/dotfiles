6b12234b4311f86104bc5b679da8c950dd93ad24
osama <iosamaify@gmail.com>
2022-08-08 09:29:01 +0200

    nvim: add treesitter-context

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,3 +1,5 @@
+local nnoremap = require("x.keymap").nnoremap
+
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "all",
 	sync_install = false,
@@ -6,3 +8,36 @@ require("nvim-treesitter.configs").setup({
 		additional_vim_regex_highlighting = false,
 	},
 })
+
+function ContextSetup(show_all_context)
+	require("treesitter-context").setup({
+		enable = true,
+		throttle = true,
+		max_lines = 0,
+		show_all_context = show_all_context,
+		patterns = {
+			default = {
+				"function",
+				"method",
+				"for",
+				"while",
+				"if",
+				"switch",
+				"case",
+			},
+			rust = {
+				"loop_expression",
+				"impl_item",
+			},
+			typescript = {
+				"class_declaration",
+				"abstract_class_declaration",
+				"else_clause",
+			},
+		},
+	})
+end
+
+nnoremap("<leader>cf", function() ContextSetup(true) end)
+nnoremap("<leader>cd", function() ContextSetup(false) end)
+ContextSetup(false)
