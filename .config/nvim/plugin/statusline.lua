b86a6ad78f01e15383f025255f6a4a1549695c40
osama <iosamaify@gmail.com>
2022-09-02 17:04:40 +0200

    nvim: fix statusline

--- .config/nvim/plugin/statusline.lua
+++ .config/nvim/plugin/statusline.lua
@@ -1,15 +1,14 @@
-local left = "%f %r%m"
+local left = "%f %r%m%{get(b:, 'gitbranch', '')}"
 local right = "%y %{&fileencoding ? &fileencoding : &encoding} %{&fileformat} | %l:%c %p%%"
 
-vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
+vim.api.nvim_create_autocmd("BufEnter", {
 	group = vim.api.nvim_create_augroup("statusline", {}),
 	pattern = "*",
 	callback = function()
-		vim.b.gitbranch = vim.fn.trim(vim.fn.system("git symbolic-ref HEAD --short 2>/dev/null"))
-		vim.o.statusline = left
-		if vim.b.gitbranch ~= nil and vim.b.gitbranch ~= "" then
-			vim.o.statusline = vim.o.statusline .. "(%{b:gitbranch})"
+		local br = vim.fn.trim(vim.fn.system("git symbolic-ref HEAD --short 2>/dev/null"))
+		if br ~= nil and br ~= "" then
+			vim.b.gitbranch = "(" .. br .. ")"
 		end
-		vim.o.statusline = vim.o.statusline .. "%=" .. right
+		vim.o.statusline = left .. "%=" .. right
 	end,
 })
