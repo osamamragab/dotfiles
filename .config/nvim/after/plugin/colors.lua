f92c2d85872cfb5966e57475cea5f37910c8b768
osama <iosamaify@gmail.com>
2022-09-17 08:21:09 +0200

    nvim: update colorscheme config

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,4 +1,3 @@
-vim.g.colors_name = "nord"
 vim.opt.background = "dark"
 vim.opt.termguicolors = true
 
@@ -7,23 +6,10 @@ vim.g.nord_italic = 1
 vim.g.nord_italic_comments = 1
 vim.g.nord_uniform_status_lines = 1
 vim.g.nord_uniform_diff_background = 1
-vim.g.nord_cursor_line_number_background = 1
+vim.g.nord_bold_vertical_split_line = 0
 
-vim.g.sonokai_style = "atlantis"
--- vim.g.sonokai_better_performance = 1
-vim.g.sonokai_transparent_background = 1
-vim.g.sonokai_diagnostic_virtual_text = "colored"
+vim.cmd("colorscheme nord")
 
-vim.g.gruvbox_contrast_dark = "hard"
-vim.g.gruvbox_sign_column = "dark0_hard"
-vim.g.gruvbox_invert_selection = 0
-vim.g.gruvbox_bold = 0
-
-local hl = function(name, opts)
-	vim.api.nvim_set_hl(0, name, opts)
+for _, name in ipairs({"Normal", "SignColumn", "CursorLineNr", "ColorColumn"}) do
+	vim.api.nvim_set_hl(0, name, { bg = "NONE" })
 end
-
-hl("Normal", { bg = "none" })
-hl("SignColumn", { bg = "none" })
-hl("CursorLineNr", { bg = "none" })
-hl("ColorColumn", { bg = "none", ctermbg = 0 })
