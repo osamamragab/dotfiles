0551016f4dc3991e1d8b3172d953e9682595cb12
osama <iosamaify@gmail.com>
2023-05-13 17:29:11 +0300

    sysinit: add apl & j

--- .config/nvim/lua/x/packer.lua
+++ .config/nvim/lua/x/packer.lua
@@ -34,6 +34,7 @@ return require("packer").startup(function(use)
 	use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true }})
 
 	-- use("tpope/vim-fugitive")
+	-- use("ziglang/zig.vim")
 	-- use("rust-lang/rust.vim")
 	-- use("darrikonn/vim-gofmt", { run = ":GoUpdateBinaries" })
 	-- use("psf/black", { tag = "stable" })
