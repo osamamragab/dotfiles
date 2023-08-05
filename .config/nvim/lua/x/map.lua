dd1aeca81f106dfde6ccc75ce3789a919006c736
Osama Ragab <theosamaragab@gmail.com>
2023-08-05 08:58:54 +0300

    neovim: map <leader>Y to act like neovim's Y

--- .config/nvim/lua/x/map.lua
+++ .config/nvim/lua/x/map.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set("x", "<leader>p", '"_dP')
 vim.keymap.set({"n", "v"}, "<leader>y", '"+y')
-vim.keymap.set("n", "<leader>Y", '"+Y')
+vim.keymap.set("n", "<leader>Y", '"+y$')
 vim.keymap.set({"n", "v"}, "<leader>d", '"_d')
 
 vim.keymap.set("n", "n", "nzzzv")
