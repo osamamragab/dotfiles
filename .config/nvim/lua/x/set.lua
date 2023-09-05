54e64b4270217dd7b595c209674a14a9d8105f65
Osama Ragab <theosamaragab@gmail.com>
2023-09-05 23:16:04 +0300

    nvim: disable node and python

--- .config/nvim/lua/x/set.lua
+++ .config/nvim/lua/x/set.lua
@@ -39,7 +39,7 @@ vim.g.netrw_fastbrowse = false
 
 vim.g.zig_fmt_autosave = false
 
-vim.g.python3_host_prog = vim.fn.expand("$PYENV_ROOT/versions/pynvim/bin/python")
-vim.g.node_host_prog = vim.fn.expand("$PNPM_HOME/neovim-node-host")
+vim.g.loaded_node_provider = false
 vim.g.loaded_ruby_provider = false
 vim.g.loaded_perl_provider = false
+vim.g.loaded_python3_provider = false
