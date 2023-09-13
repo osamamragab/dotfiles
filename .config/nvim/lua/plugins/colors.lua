9c060c52fa60c3acc12395b4fb2ab7312b8e096d
Osama Ragab <theosamaragab@gmail.com>
2023-09-13 14:25:08 +0300

    nvim: update theme and statusline

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,20 +1,19 @@
 return {
 	{
-		"nordtheme/vim",
-		name = "nord",
+		"gbprod/nord.nvim",
 		config = function()
-			vim.g.colors_name = "nord"
-			vim.g.nord_bold = true
-			vim.g.nord_italic = true
-			vim.g.nord_italic_comments = true
-			vim.g.nord_uniform_status_lines = true
-			vim.g.nord_uniform_diff_background = true
-			vim.g.nord_bold_vertical_split_line = false
-			vim.cmd.colorscheme(vim.g.colors_name)
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-			vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-			vim.api.nvim_set_hl(0, "StatusLine", { bg = vim.api.nvim_get_hl(0, { name = "StatusLine" }).bg })
+			require("nord").setup({
+				transparent = true,
+				errors = { mode = "fg" },
+				on_highlights = function(hi, c)
+					hi["@error"].undercurl = false
+					hi["StatusLine"].fg = c.none
+					hi["@parameter"].fg = c.none
+					hi["@text.title"].fg = c.none
+					hi["@punctuation.bracket"].fg = c.none
+				end
+			})
+			vim.cmd.colorscheme("nord")
 		end,
 	},
 }
--- .config/nvim/plugin/statusline.lua
+++ .config/nvim/plugin/statusline.lua
@@ -1,10 +1,7 @@
-local left = "%f %r%m"
-local right = "%y %{&fileencoding ? &fileencoding : &encoding} %{&fileformat} | %l:%c %p%%"
-
 vim.api.nvim_create_autocmd("BufEnter", {
 	group = vim.api.nvim_create_augroup("statusline", {}),
 	pattern = "*",
 	callback = function()
-		vim.opt.statusline = left .. "%=" .. right
+		vim.opt.statusline = "%f %r%m%=%y %{&fileencoding ? &fileencoding : &encoding} %{&fileformat} | %l:%c %p%%"
 	end,
 })
