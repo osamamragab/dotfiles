b11030ea41a9823e2ac31ff59d19bb85eac483f2
Osama Ragab <theosamaragab@gmail.com>
2023-09-28 07:32:47 +0300

    set tabs to 4 spaces

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -19,9 +19,9 @@ vim.opt.colorcolumn = "80"
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = false
 vim.opt.smartindent = true
 
