aef378f3385d9f3f53b1b7a7b1d0ecee3971da9d
Osama Ragab <theosamaragab@gmail.com>
2023-12-04 09:53:04 +0200

    nvim: use gruvbox-baby theme

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,19 +1,12 @@
 return {
 	{
-		"gbprod/nord.nvim",
+		"luisiacc/gruvbox-baby",
+		lazy = false,
+		priority = 1000,
+		opts = {},
 		config = function()
-			require("nord").setup({
-				transparent = true,
-				errors = { mode = "fg" },
-				on_highlights = function(hi, c)
-					hi["@error"].undercurl = false
-					hi["StatusLine"].fg = c.none
-					hi["@parameter"].fg = c.none
-					hi["@text.title"].fg = c.none
-					hi["@punctuation.bracket"].fg = c.none
-				end
-			})
-			vim.cmd.colorscheme("nord")
+			vim.g.gruvbox_baby_transparent_mode = true
+			vim.cmd.colorscheme("gruvbox-baby")
 		end,
 	},
 }
