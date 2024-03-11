38f2284ab0c6195b1b3bb2c8ba2a0b941c16267a
Osama Ragab <theosamaragab@gmail.com>
2024-03-11 18:26:58 +0200

    nvim: add luasnip

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -6,10 +6,17 @@ return {
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-nvim-lua",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
 		},
 		config = function()
 			local cmp = require("cmp")
 			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
 					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
@@ -21,12 +28,14 @@ return {
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "nvim_lua" },
+					{ name = "luasnip" },
 					{ name = "orgmode" },
 				}, {
 					{ name = "buffer", keyword_length = 5 },
 					{ name = "path" },
 				}),
 			})
+			require("luasnip.loaders.from_vscode").lazy_load()
 		end,
 	},
 }
