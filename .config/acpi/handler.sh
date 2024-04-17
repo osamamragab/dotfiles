61c8169af82d7c3792f1b1de2f4a26e7760d83b2
Osama Ragab <theosamaragab@gmail.com>
2024-04-17 23:44:38 +0200

    acpi: update handler script

--- .config/acpi/handler.sh
+++ .config/acpi/handler.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
+# Default acpi script that takes an entry for all actions
 
-minspeed=$(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq)
-maxspeed=$(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq)
+# NOTE: This is a 2.6-centric script.  If you use 2.4.x, you'll have to
+#       modify it to not use /sys
+
+# $1 should be + or - to step up or down the brightness.
+#step_backlight() {
+#    for backlight in /sys/class/backlight/*/; do
+#        [ -d "$backlight" ] || continue
+#        step=$(( $(cat "$backlight/max_brightness") / 20 ))
+#        [ "$step" -gt "1" ] || step=1 #fallback if gradation is too low
+#        printf '%s' "$(( $(cat "$backlight/brightness") $1 step ))" >"$backlight/brightness"
+#    done
+#}
+
+minspeed="/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"
+maxspeed="/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"
 setspeed="/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed"
 
 case "$1" in
@@ -11,7 +25,7 @@ case "$1" in
 				logger "PowerButton pressed: $2, shutting down..."
 				shutdown -P now
 				;;
-			*) logger "ACPI action undefined: $2" ;;
+			*)  logger "ACPI action undefined: $2" ;;
 		esac
 		;;
 	button/sleep)
@@ -28,8 +42,14 @@ case "$1" in
 		case "$2" in
 			AC|ACAD|ADP0)
 				case "$4" in
-					00000000) printf '%s' "$minspeed" >"$setspeed" ;;
-					00000001) printf '%s' "$maxspeed" >"$setspeed" ;;
+					00000000)
+						cat "$minspeed" >"$setspeed"
+						#/etc/laptop-mode/laptop-mode start
+						;;
+					00000001)
+						cat "$maxspeed" >"$setspeed"
+						#/etc/laptop-mode/laptop-mode stop
+						;;
 				esac
 				;;
 			*) logger "ACPI action undefined: $2" ;;
@@ -39,11 +59,14 @@ case "$1" in
 		case "$2" in
 			BAT0)
 				case "$4" in
-					00000000) ;;
-					00000001) ;;
+					00000000)   #echo "offline" >/dev/tty5
+						;;
+					00000001)   #echo "online"  >/dev/tty5
+						;;
 				esac
 				;;
-			CPU0) ;;
+			CPU0)
+				;;
 			*) logger "ACPI action undefined: $2" ;;
 		esac
 		;;
@@ -54,11 +77,19 @@ case "$1" in
 				logger "LID closed, suspending..."
 				zzz
 				;;
-			open) logger "LID opened" ;;
-			*) logger "ACPI action undefined (LID): $2" ;;
+			open)
+				logger "LID opened"
+				;;
+			*) logger "ACPI action undefined (LID): $2";;
 		esac
 		;;
-	video/brightnessdown) ;;
-	video/brightnessup );;
-	*) logger "ACPI group/action undefined: $1 / $2" ;;
+	video/brightnessdown)
+		#step_backlight -
+		;;
+	video/brightnessup)
+		#step_backlight +
+		;;
+	*)
+		logger "ACPI group/action undefined: $1 / $2"
+		;;
 esac
