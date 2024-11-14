76e9d65a89b7920bc0f2d217deec8084408f211e
Osama Ragab <theosamaragab@gmail.com>
2024-11-14 21:15:50 +0200

    nvim: update trouble config

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -3,21 +3,27 @@ return {
 		"folke/trouble.nvim",
 		config = function()
 			local trouble = require("trouble")
-			trouble.setup({
-				icons = false,
-				fold_open = "<",
-				fold_closed = ">",
-				indent_lines = true,
-				use_diagnostic_signs = true,
-			})
-			vim.keymap.set("n", "<leader>xw", function()
-				trouble.open("workspace_diagnostics")
+			trouble.setup({})
+			vim.keymap.set("n", "<leader>xx", function()
+				trouble.toggle("diagnostics")
 			end)
-			vim.keymap.set("n", "<leader>xd", function()
-				trouble.open("document_diagnostics")
+			vim.keymap.set("n", "<leader>xl", function()
+				trouble.toggle("loclist")
 			end)
 			vim.keymap.set("n", "<leader>xq", function()
-				trouble.open("quickfix")
+				trouble.toggle("qflist")
+			end)
+			vim.keymap.set("n", "<leader>xn", function()
+				trouble.next({
+					jump = true,
+					skip_groups = true,
+				})
+			end)
+			vim.keymap.set("n", "<leader>xp", function()
+				trouble.preousv({
+					jump = true,
+					skip_groups = true,
+				})
 			end)
 		end,
 	},
