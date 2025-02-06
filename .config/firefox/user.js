b8b2af014046971ce55241a9d68b7b85d332073e
Osama Ragab <theosamaragab@gmail.com>
2025-02-06 09:05:50 +0200

    firefox: enable search suggestions

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -3,6 +3,8 @@ user_pref("browser.startup.page", 3);
 user_pref("browser.theme.content-theme", 2);
 user_pref("browser.tabs.drawInTitlebar", true);
 user_pref("browser.startup.couldRestoreSession.count", 3);
+user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.urlbar.suggest.searches", true);
 user_pref("privacy.sanitize.sanitizeOnShutdown", false);
 user_pref("privacy.resistFingerprinting", false);
 user_pref("privacy.resistFingerprinting.letterboxing", false);
