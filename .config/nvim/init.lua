dc5694d72cd798698d6c29dafddd2b1395df646a
Osama Ragab <theosamaragab@gmail.com>
2025-02-20 09:20:28 +0200

    nvim: movie lazy plugins to its own directory

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,2 @@
-require("config.options")
-require("config.globals")
-require("config.keymaps")
-require("config.autocmd")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
+require("config")
+require("plugins")
