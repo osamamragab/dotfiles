716ae7aab2be9590d50789512cba868e74de6d7e
Osama Ragab <theosamaragab@gmail.com>
2025-03-04 05:46:24 +0200

    nvim: add zk plugin

---
+++ .config/nvim/lua/plugins/lazy/zk.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"zk-org/zk-nvim",
+		config = function()
+			require("zk").setup({
+				picker = "telescope",
+				lsp = {
+					config = {
+						cmd = { "zk", "lsp" },
+						name = "zk",
+					},
+					auto_attach = {
+						enabled = true,
+						filetypes = { "markdown" },
+					},
+				},
+			})
+		end
+	},
+}
