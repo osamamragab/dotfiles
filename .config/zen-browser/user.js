bd86bbbf5b68b53134446c88977b52e4cfc234ef
Osama Ragab <theosamaragab@gmail.com>
2025-03-11 21:15:54 +0200

    feat: replace firefox with zen browser

---
+++ .config/zen-browser/user.js
@@ -0,0 +1,8 @@
+user_pref("zen.theme.accent-color", "#3498db");
+user_pref("zen.tabs.vertical.right-side", false);
+user_pref("zen.tabs.show-newtab-vertical", false);
+user_pref("zen.tab-unloader.timeout-minutes", 60);
+user_pref("zen.view.show-newtab-button-top", false);
+user_pref("zen.view.compact.hide-toolbar", true);
+user_pref("zen.view.compact.toolbar-flash-popup", false);
+user_pref("zen.theme.color-prefs.use-workspace-colors", true);
