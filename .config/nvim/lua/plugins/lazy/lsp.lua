be404540b60df0914cb0935e679cdb9c8118a82e
Osama Ragab <theosamaragab@gmail.com>
2025-05-16 22:16:56 +0300

    stuff

--- .config/nvim/lua/plugins/lazy/lsp.lua
+++ .config/nvim/lua/plugins/lazy/lsp.lua
@@ -78,9 +78,7 @@ return {
 							capabilities = capabilities,
 							on_init = function(client)
 								local path = client.workspace_folders[1].name
-								if
-									vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc")
-								then
+								if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
 									return
 								end
 								client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
