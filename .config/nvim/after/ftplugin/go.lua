a10f8b8762ddf255fbfac58b0c4c0cff15887404
Osama Ragab <theosamaragab@gmail.com>
2026-06-08 07:03:00 +0300

    feat(nvim): add autocmd for zls code actions

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -1,4 +1,4 @@
-local augroup = vim.api.nvim_create_augroup("gofmt", { clear = true })
+local augroup = vim.api.nvim_create_augroup("gopls", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	group = augroup,
@@ -6,13 +6,13 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	callback = function(_)
 		vim.lsp.buf.format()
 		vim.lsp.buf.code_action({
+			apply = true,
 			context = {
 				diagnostics = {},
 				only = {
 					"source.organizeImports",
 				},
 			},
-			apply = true,
 		})
 	end,
 })
--- .config/nvim/after/ftplugin/zig.lua
+++ .config/nvim/after/ftplugin/zig.lua
@@ -1,3 +1,22 @@
 vim.opt_local.expandtab = true
 vim.g.zig_fmt_autosave = false
 vim.g.zig_recommended_style = true
+
+local augroup = vim.api.nvim_create_augroup("zls", { clear = true })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	group = augroup,
+	pattern = { "*.zig", "*.zon" },
+	callback = function(_)
+		vim.lsp.buf.code_action({
+			apply = true,
+			context = {
+				diagnostics = {},
+				only = {
+					"source.fixAll",
+					"source.organizeImports",
+				},
+			},
+		})
+	end
+})
