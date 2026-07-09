2b9c04a5cb62d8bdbb8d774c667cd85c22236c06
Osama Ragab <theosamaragab@gmail.com>
2026-07-09 21:55:50 +0300

    fix: prevent home-manager from creating ~/.parallel directory

--- modules/home/programs/parallel.nix
+++ modules/home/programs/parallel.nix
@@ -1,8 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 {
     programs.parallel = {
         enable = true;
         package = pkgs.parallel;
-        will-cite = true;
+        will-cite = false; # disabled because it creates ~/.parallel by default
+    };
+
+    home.sessionVariables.PARALLEL_HOME = lib.mkIf config.programs.parallel.enable "${config.xdg.dataHome}/parallel";
+    home.file."${config.home.sessionVariables.PARALLEL_HOME}/will-cite" = lib.mkIf config.programs.parallel.enable {
+        text = "";
     };
 }
