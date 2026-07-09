d6897d1aa74b4ff026c9795dfba49ce9685c66a9
Osama Ragab <theosamaragab@gmail.com>
2026-07-10 01:38:16 +0300

    feat: update emacs config

--- modules/home/programs/emacs.nix
+++ modules/home/programs/emacs.nix
@@ -1,11 +1,19 @@
 { pkgs, lib, config, ... }:
+let
+    configPath = ../files/emacs;
+in
 {
     programs.emacs = {
         enable = true;
         package = pkgs.emacs-pgtk;
     };
-    home.file.".config/emacs" = lib.mkIf config.programs.emacs.enable {
-        source = ../files/emacs;
-        recursive = true;
+    home.file.".config/emacs" =
+        lib.mkIf (config.programs.emacs.enable && builtins.pathExists configPath)
+        {
+            source = configPath;
+            recursive = true;
+        };
+    home.shellAliases = lib.mkIf config.programs.emacs.enable {
+        emacs = "emacsclient -nca emacs";
     };
 }
