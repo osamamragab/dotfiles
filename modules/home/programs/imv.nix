196ea4e4f5c5c0febbf976f0b423d1f6680b34a3
Osama Ragab <theosamaragab@gmail.com>
2026-07-15 11:22:32 +0300

    fix(imv): fix configuration

--- modules/home/programs/imv.nix
+++ modules/home/programs/imv.nix
@@ -1,5 +1,6 @@
 {
     pkgs,
+    config,
     ...
 }:
 {
@@ -8,7 +9,10 @@
         package = pkgs.imv;
         settings = {
             options = {
-                suppress_default_binds = true;
+                # NOTE: home-manager sets binds before options, and imv has
+                # a bug (?) where it clears all bindings (including user-defined)
+                # set before suppress_default_binds option; disabling it for now.
+                # suppress_default_binds = true;
                 background = "2e3440";
                 overlay = "true";
                 overlay_font = "monospace:14";
@@ -17,55 +21,91 @@
                 overlay_background_alpha = "ff";
                 overlay_position_bottom = true;
             };
-            binds = {
-                q = "quit";
-                x = "close";
-                f = "fullscreen";
-                n = "next";
-                p = "prev";
-                "<Left>" = "prev";
-                "<bracketleft>" = "prev";
-                "<Right>" = "next";
-                "<bracketright>" = "next";
-                gg = "goto 1";
-                "<Shift+G>" = "goto -1";
-                j = "pan 0 -50";
-                k = "pan 0 50";
-                h = "pan 50 0";
-                l = "pan -50 0";
-                "<Up>" = "zoom 1";
-                "<Shift+plus>" = "zoom 1";
-                i = "zoom 1";
-                o = "zoom -1";
-                "<Down>" = "zoom -1";
-                "<minus>" = "zoom -1";
-                r = "rotate by 90";
-                "<Shift+R>" = "rotate by -90";
-                d = "overlay";
-                "<Shift+P>" = ''exec echo "$imv_current_file"'';
-                c = "center";
-                s = "scaling next";
-                "<Shift+S>" = "upscaling next";
-                a = "zoom actual";
-                "<Ctrl+r>" = "reset";
-                "<period>" = "next_frame";
-                "<space>" = "toggle_playing";
-                t = "slideshow +1";
-                "<Shift+T>" = "slideshow -1";
-                "<Ctrl+x>w" = ''exec pkill -x wbg; exec setsid -f wbg "$imv_current_file"'';
-                "<Ctrl+x>r" = ''exec magick mogrify -rotate 90 "$imv_current_file"'';
-                "<Ctrl+x><Shift+R>" = ''exec magick mogrify -rotate -90 "$imv_current_file"'';
-                "<Ctrl+x>f" = ''exec magick mogrify -flop "$imv_current_file"'';
-                "<Ctrl+x>i" =
-                    ''exec file "$imv_current_file"; exec mediainfo "$imv_current_file"'';
-                "<Ctrl+x>d" =
-                    ''exec [ "$(printf "no\nyes" | menu -p "delete $imv_current_file?")" = "yes" ] && rm "$imv_current_file" && echo "$imv_current_file deleted" && imv-msg "$imv_pid" close'';
-                "<Ctrl+x><Shift+D>" =
-                    ''exec rm "$imv_current_file" && echo "$imv_current_file deleted" && imv-msg "$imv_pid" close'';
-                "<Ctrl+x>y" =
-                    ''exec wl-copy <"$imv_current_file" && echo "$imv_current_file copied to clipboard"'';
-                "<Ctrl+x>g" = ''exec setsid -f dragon-drop -x "$imv_current_file"'';
-            };
+            binds =
+                let
+                    # using scripts because imv cannot parse long lines (sigh).
+                    bgScript = pkgs.writeShellScript "imv-bg" ''
+                        set -eu
+                        pkill -x wbg
+                        setsid -f ${pkgs.wbg}/bin/wbg "$imv_current_file"
+                    '';
+                    infoScript = pkgs.writeShellScript "imv-info" ''
+                        set -eu
+                        info="$(${pkgs.mediainfo}/bin/mediainfo "$imv_current_file")"
+                        ${pkgs.libnotify}/bin/notify-send -a imv "$info"
+                    '';
+                    copyScript = pkgs.writeShellScript "imv-copy" ''
+                        set -eu
+                        ${pkgs.wl-clipboard}/bin/wl-copy <"$imv_current_file"
+                        ${pkgs.libnotify}/bin/notify-send -a imv "$imv_current_file copied to clipboard"
+                    '';
+                    dragScript = pkgs.writeShellScript "imv-drag" ''
+                        set -eu
+                        setsid -f ${pkgs.dragon-drop}/bin/dragon-drop -x "$imv_current_file"
+                    '';
+                    flipScript = pkgs.writeShellScript "imv-flip" ''
+                        set -eu
+                        ${pkgs.imagemagick}/bin/magick mogrify -flop "$imv_current_file"
+                    '';
+                    rotateScript = pkgs.writeShellScript "imv-rotate" ''
+                        set -eu
+                        ${pkgs.imagemagick}/bin/magick mogrify -rotate "$1" "$imv_current_file"
+                    '';
+                    deleteScript = pkgs.writeShellScript "imv-delete" ''
+                        set -eu
+                        [ "$1" = "-f" ] ||
+                            [ "$(printf "no\nyes" | menu -p "delete $imv_current_file?")" = "yes" ] ||
+                            exit 1
+                        rm "$imv_current_file"
+                        ${pkgs.libnotify}/bin/notify-send -a imv "$imv_current_file deleted"
+                        ${config.programs.imv.package}/bin/imv-msg "$imv_pid" close
+                    '';
+                in
+                {
+                    q = "quit";
+                    x = "close";
+                    f = "fullscreen";
+                    n = "next";
+                    p = "prev";
+                    "<Left>" = "prev";
+                    "<bracketleft>" = "prev";
+                    "<Right>" = "next";
+                    "<bracketright>" = "next";
+                    gg = "goto 1";
+                    "<Shift+G>" = "goto -1";
+                    j = "pan 0 -50";
+                    k = "pan 0 50";
+                    h = "pan 50 0";
+                    l = "pan -50 0";
+                    "<Up>" = "zoom 1";
+                    "<Shift+plus>" = "zoom 1";
+                    i = "zoom 1";
+                    o = "zoom -1";
+                    "<Down>" = "zoom -1";
+                    "<minus>" = "zoom -1";
+                    r = "rotate by 90";
+                    "<Shift+R>" = "rotate by -90";
+                    d = "overlay";
+                    "<Shift+P>" = ''exec echo "$imv_current_file"'';
+                    c = "center";
+                    s = "scaling next";
+                    "<Shift+S>" = "upscaling next";
+                    a = "zoom actual";
+                    "<Ctrl+r>" = "reset";
+                    "<period>" = "next_frame";
+                    "<space>" = "toggle_playing";
+                    t = "slideshow +1";
+                    "<Shift+T>" = "slideshow -1";
+                    "<Ctrl+x>w" = "exec ${builtins.toString bgScript}";
+                    "<Ctrl+x>r" = "exec ${builtins.toString rotateScript} 90";
+                    "<Ctrl+x><Shift+R>" = "exec ${builtins.toString rotateScript} -90";
+                    "<Ctrl+x>f" = "exec ${builtins.toString flipScript}";
+                    "<Ctrl+x>i" = "exec ${builtins.toString infoScript}";
+                    "<Ctrl+x>d" = "exec ${builtins.toString deleteScript}";
+                    "<Ctrl+x><Shift+D>" = "exec ${builtins.toString deleteScript} -f";
+                    "<Ctrl+x>y" = "exec ${builtins.toString copyScript}";
+                    "<Ctrl+x>g" = "exec ${builtins.toString dragScript}";
+                };
         };
     };
 }
