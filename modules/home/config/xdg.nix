f0cfa9e1f0f465317ed960987230f3f1ce7781fd
Osama Ragab <theosamaragab@gmail.com>
2026-07-16 16:33:06 +0300

    feat(xdg): remove xdg-desktop-portal-wlr config

--- modules/home/config/xdg.nix
+++ modules/home/config/xdg.nix
@@ -62,20 +62,7 @@
         };
     };
 
-    home.sessionPath = lib.mkIf config.xdg.localBinInPath [ config.xdg.binHome ];
-
-    # TODO: select window
-    xdg.configFile."xdg-desktop-portal-wlr/config" =
-        let
-            iniFormat = pkgs.formats.ini { };
-        in
-        lib.mkIf config.xdg.portal.enable {
-            source = iniFormat.generate "xdg-desktop-portal-wlr-config.ini" {
-                screencast = {
-                    max_fps = 60;
-                    chooser_type = "simple";
-                    chooser_cmd = "${pkgs.slurp}/bin/slurp -or -f 'Monitor: %o'";
-                };
-            };
-        };
+    home.sessionPath = lib.mkIf config.xdg.localBinInPath [
+        config.xdg.binHome
+    ];
 }
