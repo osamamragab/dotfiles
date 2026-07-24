37de77da3fb2b0c01c4fc7d02d22813f371dab43
Osama Ragab <theosamaragab@gmail.com>
2026-07-24 14:27:16 +0300

    feat(less): add options

--- modules/home/programs/less.nix
+++ modules/home/programs/less.nix
@@ -7,7 +7,14 @@
         enable = true;
         package = pkgs.less;
         options = {
+            quiet = true;
+            no-init = true;
+            wordwrap = true;
+            use-color = true;
+            ignore-case = true;
+            hilite-unread = true;
             RAW-CONTROL-CHARS = true;
+            quit-if-one-screen = false;
         };
     };
     programs.lesspipe = {
