36ff5dd0d52cfaadc3447d5a9a1dd01628619f78
Osama Ragab <theosamaragab@gmail.com>
2026-07-26 10:01:35 +0300

    feat(kanshi): implement relative outputs position

--- modules/home/services/kanshi.nix
+++ modules/home/services/kanshi.nix
@@ -1,8 +1,65 @@
 {
     pkgs,
+    lib,
     config,
     ...
 }:
+let
+    outputs = lib.lists.map (e: e.output) (
+        lib.lists.filter (
+            e: e ? output && e.output != { }
+        ) config.services.kanshi.settings
+    );
+    parsePos =
+        output:
+        let
+            pos = lib.strings.splitString "," (output.position or "0,0");
+        in
+        {
+            x = lib.strings.toInt (lib.lists.elemAt pos 0);
+            y = lib.strings.toInt (lib.lists.elemAt pos 1);
+        };
+    parseDims =
+        output:
+        let
+            dims = lib.strings.splitString "x" (output.mode or "0x0");
+            scale = output.scale or 1.0;
+            width = lib.strings.toInt (lib.lists.elemAt dims 0);
+            height = lib.strings.toInt (lib.lists.elemAt dims 1);
+        in
+        {
+            w = lib.floor (width / scale);
+            h = lib.floor (height / scale);
+        };
+    relativePos =
+        rel: criteria:
+        let
+            output = lib.lists.findFirst (e: e.criteria == criteria) { } outputs;
+            dims = parseDims output;
+            pos = parsePos output;
+            x = pos.x + dims.w;
+            y = pos.y + dims.h;
+            newPos = lib.attrsets.getAttr rel {
+                left = {
+                    x = x;
+                    y = pos.y;
+                };
+                right = {
+                    x = -x;
+                    y = pos.y;
+                };
+                top = {
+                    x = pos.x;
+                    y = y;
+                };
+                bottom = {
+                    x = pos.x;
+                    y = -y;
+                };
+            };
+        in
+        "${lib.toString newPos.x},${lib.toString newPos.y}";
+in
 {
     services.kanshi = {
         enable = true;
@@ -21,7 +78,7 @@
                 output = {
                     criteria = "HDMI-A-1";
                     mode = "1920x1200";
-                    position = "1536,0";
+                    position = relativePos "left" "eDP-1";
                     scale = 1.0;
                 };
             }
@@ -29,7 +86,7 @@
                 output = {
                     criteria = "DP-2";
                     mode = "1680x1050";
-                    position = "3456,0";
+                    position = relativePos "left" "HDMI-A-1";
                     scale = 1.0;
                 };
             }
