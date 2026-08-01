9e443db539deadd62c47a9df95e53b433c8ad3a5
Osama Ragab <theosamaragab@gmail.com>
2026-08-02 02:17:13 +0300

    feat(xlab): add user to input group

--- hosts/xlab/default.nix
+++ hosts/xlab/default.nix
@@ -149,6 +149,7 @@
                     pkgs.bashInteractive;
             extraGroups = [
                 "wheel"
+                "input"
                 "video"
                 "audio"
                 "dialout"
